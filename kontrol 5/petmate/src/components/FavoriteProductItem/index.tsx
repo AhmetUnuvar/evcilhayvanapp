1f0a0064a23791d3cffd199967393c371c09c86d
AhmetUnuvar <95508776+AhmetUnuvar@users.noreply.github.com>
2024-04-25 21:42:44 +0300

    kontrol 5
    5. kontrol

---
+++ kontrol 5/petmate/src/components/FavoriteProductItem/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, TouchableOpacity, Text, Image } from "react-native";
+import { Product } from "../../models";
+import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+
+type ProductProps = {
+  product: Product;
+};
+
+function Index({ product }: ProductProps) {
+  return (
+    <TouchableOpacity style={styles.favorite}>
+      <View style={styles.favoriteView}>
+        <Image
+          source={{ uri: product.image }}
+          resizeMode="stretch"
+          style={styles.favoriteImage}
+        />
+        <View style={styles.labeledFav}>
+          <Text style={{ fontSize: 9, fontWeight: "500" }}>Acil İlanlar</Text>
+        </View>
+        <TouchableOpacity>
+          <AntDesign
+            style={{ position: "absolute", right: 4, bottom: 5 }}
+            name="heart"
+            size={24}
+            color="white"
+          />
+        </TouchableOpacity>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default Index;
