50e1d59a9756dc853f594afaca8d2c8d5656314f
AhmetUnuvar <95508776+AhmetUnuvar@users.noreply.github.com>
2024-06-02 02:40:18 +0300

    kontrol 9
    kontrol 9 . 02.06.2024 tarihinde commit attım. veri tabanına başladım.

---
+++ kontrol 9/petmate/src/components/MainProducts/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  productsContainer: {
+    width: "92%",
+    marginHorizontal: "4%",
+    backgroundColor: "red",
+  },
+});
+export default styles;
